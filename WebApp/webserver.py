ed2b8e2019078002c4cebc4b6ff770da53c6cccd
LanderDeRoeck <Lander.DeRoeck@student.uantwerpen.be>
2019-11-04 17:40:30 +0100

    Removed redirect

--- WebApp/webserver.py
+++ WebApp/webserver.py
@@ -12,7 +12,7 @@ app_data = dict()
 ## Custom error ##
 @app.errorhandler(404)
 def render_404(errormsg):
-    return redirect("/")
+    return errormsg
 
 @app.errorhandler(401)
 def render_401(errormsg):
@@ -27,13 +27,16 @@ def home():
 def manual():
     return render_template("manual.html")
 
+# Used for site
 api.add_resource(restful_api.GetAllLines, "/api/all-lines")
-api.add_resource(restful_api.GetLineInfo, "/api/line/<int:entiteitnummer>/<int:lijnnummer>")
-api.add_resource(restful_api.GetHandledStops, "/api/stops/<int:entiteitnummer>/<int:lijnnummer>/<string:richting>")
 api.add_resource(restful_api.GetStopInformation, "/api/stop/<int:entiteitnummer>/<int:haltenummer>")
 api.add_resource(restful_api.GetRealtimeInfo, "/api/real-time/<int:entiteitnummer>/<int:lijnnummer>/<string:richting>")
 api.add_resource(restful_api.GetBusUpdate, "/api/update/<int:entiteitnummer>/<int:lijnnummer>/<string:richting>")
+
+# Legacy endpoints, used for testing
+api.add_resource(restful_api.GetHandledStops, "/api/stops/<int:entiteitnummer>/<int:lijnnummer>/<string:richting>")
 api.add_resource(restful_api.GetRoute, "/api/routing/<int:entiteitnummer>/<int:lijnnummer>/<string:richting>")
+api.add_resource(restful_api.GetLineInfo, "/api/line/<int:entiteitnummer>/<int:lijnnummer>")
 
 if __name__ == "__main__":
     app.env = "development"
